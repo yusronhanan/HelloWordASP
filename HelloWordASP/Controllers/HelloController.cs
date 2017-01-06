da824886ad6f1e16afa56ca3fa3ab5a8688c1468
Miftahul Rohmah <miftahul_rohmah_24rpl@student.smktelkom-mlg.sch.id>
2017-01-06 15:52:27 +0700

    Add project files.

---
+++ HelloWordASP/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWordASP.Controllers
+{
+    public class HelloController : Controller
+    {
+        //
+        // GET: /Hello/
+
+        public ActionResult HelloWorld()
+        {
+            return View();
+        }
+
+    }
+}
